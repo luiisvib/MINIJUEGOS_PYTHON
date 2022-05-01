7a7e5de63a95451b469029c940ff31ba0bb2f956
Federico <isma-fb@hotmail.com>
2022-05-01 20:00:14 +0200

    Subo inicio del ratagotchi

---
+++ tamagotchi.py
@@ -0,0 +1,73 @@
+import os
+import time
+
+class Ratagotchi:
+    def __init__(self, nombre, vida=0, hambre=50, higiene=100, felicidad=100):
+        self.nombre = nombre
+        self.vida = vida
+        self.hambre = hambre
+        self.higiene = higiene
+        self.felicidad = felicidad
+
+    def __str__(self):
+        return 'Nombre: {}\nVida: {}\nHambre: {}\nHigiene: {}\nFelicidad: {}'.format(self.nombre, self.vida, self.hambre, self.higiene, self.felicidad)
+
+    def alimentar(self):
+        print('La rata ' + self.nombre + ' esta comiendo.')
+        self.hambre += 20
+        if self.hambre == 100:
+            self.vida += 20
+            self.hambre = 100
+        
+    def jugar(self):
+        print('Jugaste con la rata ' + self.nombre)
+        self.felicidad += 20
+        if self.felicidad > 100:
+            self.felicidad = 100
+
+    def banyar(self):
+        print('Has bañado a la rata ' + self.nombre)
+        self.higiene += 50
+        if self.higiene > 100:
+            self.higiene = 100
+
+    def muerte(self):
+        if self.vida > 100 or self.hambre <= 0 or self.felicidad <= 0:
+            return True
+        else: 
+            return False
+
+    def cuidados(self, accion):
+        # self.vida -= 10
+        self.hambre -= 10
+        self.felicidad -= 10
+        self.higiene -= 10
+        if accion == 'alimentar':
+            self.alimentar()
+        elif accion == 'jugar':
+            self.jugar()
+        elif accion == 'bañar':
+            self.banyar()
+
+
+def menu():
+    nombre = input('Ponle un nombre a la rata: ')
+    rata = Ratagotchi(nombre)
+
+    while not rata.muerte():
+        print('\n'+ str(rata))
+        print('\n¿Qué quieres hacer? ')
+        print('\n1. Alimentar\n2. Jugar\n3. Bañar\n0. Salir\n')
+        opc = int(input('Seleccionar: '))
+        if opc == 1:
+            rata.cuidados('alimentar')
+        elif opc == 2:
+            rata.cuidados('jugar')
+        elif opc == 3:
+            rata.cuidados('bañar')
+        elif opc == 0:
+            print('Chauuu')
+            break
+    print(rata.nombre + ' murio.')
+
+menu()
